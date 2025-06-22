72806474d8690c4ca3e66b3e3544bef9f17e9e5d
Somnath Rankhambe <somnathrankhambe3@gmail.com>
2025-06-22 13:58:48 +0530

    Add files via upload

---
+++ musiclibrary.py
@@ -0,0 +1,7 @@
+music = {
+    "skyfall" : "https://www.youtube.com/watch?v=sZrTJesvJeo&list=RDsZrTJesvJeo&start_radio=1&pp=ygUHc2t5ZmFsbKAHAQ%3D%3D",
+    "Jugraafiya" : "https://www.youtube.com/watch?v=rS4G5az-MKA&list=RDrS4G5az-MKA&start_radio=1",
+    "ChaarKadam" : "https://www.youtube.com/watch?v=WKbwopSXLWU&list=RDWKbwopSXLWU&start_radio=1",
+    "HuppaHuiyya" : "https://www.youtube.com/watch?v=Nz2oNWUFaJw&list=RDNz2oNWUFaJw&start_radio=1"
+
+}
